83a738de50458e168c92d8fd0ca8ecdab6b0b4f0
Dylan Bailes <dbailes0001@gmail.com>
2026-07-20 08:45:36 -0700

    local push

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,6 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:macro_tracker/app/macro_tracker_app.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
